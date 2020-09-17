a95a040bb128c105fd361ccdc35d3dac6b0840cf
RobotLionIDE <kozlov.gv@robot-lion.ru>
2020-09-17 16:21:21 +0300

    h-cppi

--- include/mgos_arduino_sh1106.h
+++ include/mgos_arduino_sh1106.h
@@ -21,162 +21,167 @@
 class Adafruit_SH1106;
 class TwoWire;
 
-bool mgos_arduino_sh1106_init(void);
+extern "C"
+{
 
-/*
- * Create and return an Adafruit_SH1106 instance for I2C.
- * - w   - display width
- * - h   - display height
- * - i2c - i2c channel
- */
-Adafruit_SH1106 *mgos_sh1106_create_i2c(int w, int h);
+    bool mgos_arduino_sh1106_init(void);
 
-/*
- * Close Adafruit_SH1106 indstance.
- */
-void mgos_sh1106_close(Adafruit_SH1106 *ssd);
+    /*
+    * Create and return an Adafruit_SH1106 instance for I2C.
+    * - w   - display width
+    * - h   - display height
+    * - i2c - i2c channel
+    */
+    Adafruit_SH1106 *mgos_sh1106_create_i2c(int w, int h);
 
-/*
- * Initialize the display. `vccstate` is a VCC state, one of those:
- * - `SH1106_EXTERNALVCC`
- * - `SH1106_SWITCHCAPVCC`
- * `i2caddr` is an I2C address (ignored if `create_spi` was used).
- * If `reset` is true, then the display controller will be reset.
- * Example:
- * ```javascript
- * mySH1106 = mgos_sh1106_create_i2c(12, MGOS_SH1106_RES_96_16);
- * mgos_sh1106_begin(mySH1106, SH1106_EXTERNALVCC, 0x42, true);
- * ```
- */
-void mgos_sh1106_begin(Adafruit_SH1106 *ssd, int vccstate, int i2caddr);
+    /*
+    * Close Adafruit_SH1106 indstance.
+    */
+    void mgos_sh1106_close(Adafruit_SH1106 *ssd);
 
-/*
- * Send an arbitrary command `cmd`, which must be a number from 0 to 255.
- */
-void mgos_sh1106_command(Adafruit_SH1106 *ssd, int c);
+    /*
+    * Initialize the display. `vccstate` is a VCC state, one of those:
+    * - `SH1106_EXTERNALVCC`
+    * - `SH1106_SWITCHCAPVCC`
+    * `i2caddr` is an I2C address (ignored if `create_spi` was used).
+    * If `reset` is true, then the display controller will be reset.
+    * Example:
+    * ```javascript
+    * mySH1106 = mgos_sh1106_create_i2c(12, MGOS_SH1106_RES_96_16);
+    * mgos_sh1106_begin(mySH1106, SH1106_EXTERNALVCC, 0x42, true);
+    * ```
+    */
+    void mgos_sh1106_begin(Adafruit_SH1106 *ssd, int vccstate, int i2caddr);
 
-/*
- * Clear display.
- */
-void mgos_sh1106_clear_display(Adafruit_SH1106 *ssd);
+    /*
+    * Send an arbitrary command `cmd`, which must be a number from 0 to 255.
+    */
+    void mgos_sh1106_command(Adafruit_SH1106 *ssd, int c);
 
-/*
- * Set invert mode: 0 - don't invert; 1 - invert.
- */
-void mgos_sh1106_invert_display(Adafruit_SH1106 *ssd, int i);
+    /*
+    * Clear display.
+    */
+    void mgos_sh1106_clear_display(Adafruit_SH1106 *ssd);
 
-/*
- * Put image data to the display.
- */
-void mgos_sh1106_display(Adafruit_SH1106 *ssd);
+    /*
+    * Set invert mode: 0 - don't invert; 1 - invert.
+    */
+    void mgos_sh1106_invert_display(Adafruit_SH1106 *ssd, int i);
 
-/*
- * Activate a right handed scroll for rows `start` through `stop`.
- */
-//void mgos_sh1106_start_scroll_right(Adafruit_SH1106 *ssd, int start,
-//                                    int stop);
+    /*
+    * Put image data to the display.
+    */
+    void mgos_sh1106_display(Adafruit_SH1106 *ssd);
 
-/*
- * Activate a left handed scroll for rows `start` through `stop`.
- */
-//void mgos_sh1106_start_scroll_left(Adafruit_SH1106 *ssd, int start, int stop);
+    /*
+    * Activate a right handed scroll for rows `start` through `stop`.
+    */
+    //void mgos_sh1106_start_scroll_right(Adafruit_SH1106 *ssd, int start,
+    //                                    int stop);
 
-/*
- * Activate a diagonal scroll for rows `start` through `stop`.
- */
-//void mgos_sh1106_start_scroll_diag_right(Adafruit_SH1106 *ssd, int start,
-//                                          int stop);
+    /*
+    * Activate a left handed scroll for rows `start` through `stop`.
+    */
+    //void mgos_sh1106_start_scroll_left(Adafruit_SH1106 *ssd, int start, int stop);
 
-/*
- * Activate a diagonal scroll for rows `start` through `stop`.
- */
-//void mgos_sh1106_start_scroll_diag_left(Adafruit_SH1106 *ssd, int start,
-//                                         int stop);
+    /*
+    * Activate a diagonal scroll for rows `start` through `stop`.
+    */
+    //void mgos_sh1106_start_scroll_diag_right(Adafruit_SH1106 *ssd, int start,
+    //                                          int stop);
 
-/*
- * Stop scroll.
- */
-//void mgos_sh1106_stop_scroll(Adafruit_SH1106 *ssd);
+    /*
+    * Activate a diagonal scroll for rows `start` through `stop`.
+    */
+    //void mgos_sh1106_start_scroll_diag_left(Adafruit_SH1106 *ssd, int start,
+    //                                         int stop);
 
-/*
- * Set dim mode:
- * dim is 1: display is dimmed;
- * dim is 0: display is normal.
- */
-//void mgos_sh1106_dim(Adafruit_SH1106 *ssd, int dim);
+    /*
+    * Stop scroll.
+    */
+    //void mgos_sh1106_stop_scroll(Adafruit_SH1106 *ssd);
 
-/*
- * Set a single pixel at `x`, `y` to have the given `color`, the color being
- * one of the following: `BLACK`, `WHITE`, `INVERSE`.
- */
-void mgos_sh1106_draw_pixel(Adafruit_SH1106 *ssd, int x, int y, int color);
+    /*
+    * Set dim mode:
+    * dim is 1: display is dimmed;
+    * dim is 0: display is normal.
+    */
+    //void mgos_sh1106_dim(Adafruit_SH1106 *ssd, int dim);
 
-/*
- * Draw a vertical line with height `h`, starting at `x`, `y`, with color
- * `color` (`BLACK`, `WHITE` or `INVERSE`)
- */
-void mgos_sh1106_draw_fast_vline(Adafruit_SH1106 *ssd, int x, int y, int h,
-                                  int color);
+    /*
+    * Set a single pixel at `x`, `y` to have the given `color`, the color being
+    * one of the following: `BLACK`, `WHITE`, `INVERSE`.
+    */
+    void mgos_sh1106_draw_pixel(Adafruit_SH1106 *ssd, int x, int y, int color);
 
-/*
- * Draw a horizontal line with width `w`, starting at `x`, `y`, with color
- * `color` (`BLACK`, `WHITE` or `INVERSE`)
- */
-void mgos_sh1106_draw_fast_hline(Adafruit_SH1106 *ssd, int x, int y, int w,
-                                  int color);
+    /*
+    * Draw a vertical line with height `h`, starting at `x`, `y`, with color
+    * `color` (`BLACK`, `WHITE` or `INVERSE`)
+    */
+    void mgos_sh1106_draw_fast_vline(Adafruit_SH1106 *ssd, int x, int y, int h,
+                                    int color);
 
-/*
- * Adafruit_GFX
- */
-int mgos_sh1106_make_xy_pair(int x, int y);
+    /*
+    * Draw a horizontal line with width `w`, starting at `x`, `y`, with color
+    * `color` (`BLACK`, `WHITE` or `INVERSE`)
+    */
+    void mgos_sh1106_draw_fast_hline(Adafruit_SH1106 *ssd, int x, int y, int w,
+                                    int color);
 
-void mgos_sh1106_draw_circle(Adafruit_SH1106 *ssd, int x0y0, int r,
-                              int color);
+    /*
+    * Adafruit_GFX
+    */
+    int mgos_sh1106_make_xy_pair(int x, int y);
 
-void mgos_sh1106_draw_circle_helper(Adafruit_SH1106 *ssd, int x0y0, int r,
-                                     int cornername, int color);
+    void mgos_sh1106_draw_circle(Adafruit_SH1106 *ssd, int x0y0, int r,
+                                int color);
 
-void mgos_sh1106_fill_circle(Adafruit_SH1106 *ssd, int x0y0, int r,
-                              int color);
+    void mgos_sh1106_draw_circle_helper(Adafruit_SH1106 *ssd, int x0y0, int r,
+                                        int cornername, int color);
 
-void mgos_sh1106_fill_circle_helper(Adafruit_SH1106 *ssd, int x0y0, int r,
-                                     int cornername, int delta, int color);
+    void mgos_sh1106_fill_circle(Adafruit_SH1106 *ssd, int x0y0, int r,
+                                int color);
 
-void mgos_sh1106_draw_triangle(Adafruit_SH1106 *ssd, int x0y0, int x1y1,
-                                int x2y2, int color);
+    void mgos_sh1106_fill_circle_helper(Adafruit_SH1106 *ssd, int x0y0, int r,
+                                        int cornername, int delta, int color);
 
-void mgos_sh1106_fill_triangle(Adafruit_SH1106 *ssd, int x0y0, int x1y1,
-                                int x2y2, int color);
+    void mgos_sh1106_draw_triangle(Adafruit_SH1106 *ssd, int x0y0, int x1y1,
+                                    int x2y2, int color);
 
-void mgos_sh1106_draw_round_rect(Adafruit_SH1106 *ssd, int x0y0, int w, int h,
-                                  int radius, int color);
+    void mgos_sh1106_fill_triangle(Adafruit_SH1106 *ssd, int x0y0, int x1y1,
+                                    int x2y2, int color);
 
-void mgos_sh1106_fill_round_rect(Adafruit_SH1106 *ssd, int x0y0, int w, int h,
-                                  int radius, int color);
+    void mgos_sh1106_draw_round_rect(Adafruit_SH1106 *ssd, int x0y0, int w, int h,
+                                    int radius, int color);
 
-void mgos_sh1106_draw_char(Adafruit_SH1106 *ssd, int xy, int c, int color,
-                            int bg, int size);
+    void mgos_sh1106_fill_round_rect(Adafruit_SH1106 *ssd, int x0y0, int w, int h,
+                                    int radius, int color);
 
-void mgos_sh1106_set_cursor(Adafruit_SH1106 *ssd, int x, int y);
+    void mgos_sh1106_draw_char(Adafruit_SH1106 *ssd, int xy, int c, int color,
+                                int bg, int size);
 
-void mgos_sh1106_set_text_color(Adafruit_SH1106 *ssd, int c);
-void mgos_sh1106_set_text_color_bg(Adafruit_SH1106 *ssd, int c, int bg);
-void mgos_sh1106_set_text_size(Adafruit_SH1106 *ssd, int s);
-void mgos_sh1106_set_text_wrap(Adafruit_SH1106 *ssd, boolean w);
+    void mgos_sh1106_set_cursor(Adafruit_SH1106 *ssd, int x, int y);
 
-int mgos_sh1106_write(Adafruit_SH1106 *ssd, const char *buffer, int size);
+    void mgos_sh1106_set_text_color(Adafruit_SH1106 *ssd, int c);
+    void mgos_sh1106_set_text_color_bg(Adafruit_SH1106 *ssd, int c, int bg);
+    void mgos_sh1106_set_text_size(Adafruit_SH1106 *ssd, int s);
+    void mgos_sh1106_set_text_wrap(Adafruit_SH1106 *ssd, boolean w);
 
-int mgos_sh1106_height(Adafruit_SH1106 *ssd);
-int mgos_sh1106_width(Adafruit_SH1106 *ssd);
+    int mgos_sh1106_write(Adafruit_SH1106 *ssd, const char *buffer, int size);
 
-int mgos_sh1106_get_rotation(Adafruit_SH1106 *ssd);
-void mgos_sh1106_set_rotation(Adafruit_SH1106 *ssd, int rotation);
+    int mgos_sh1106_height(Adafruit_SH1106 *ssd);
+    int mgos_sh1106_width(Adafruit_SH1106 *ssd);
 
-/*
- * get current cursor position (get rotation safe maximum values, using: width()
- * for x, height() for y)
- */
-int mgos_sh1106_get_cursor_x(Adafruit_SH1106 *ssd);
-int mgos_sh1106_get_cursor_y(Adafruit_SH1106 *ssd);
+    int mgos_sh1106_get_rotation(Adafruit_SH1106 *ssd);
+    void mgos_sh1106_set_rotation(Adafruit_SH1106 *ssd, int rotation);
+
+    /*
+    * get current cursor position (get rotation safe maximum values, using: width()
+    * for x, height() for y)
+    */
+    int mgos_sh1106_get_cursor_x(Adafruit_SH1106 *ssd);
+    int mgos_sh1106_get_cursor_y(Adafruit_SH1106 *ssd);
+
+}//extern "C"
 
 #endif /* CS_MOS_LIBS_ARDUINO_ADAFRUIT_SH1106_SRC_MGOS_ARDUINO_SH1106_H_ */
--- src/mgos_arduino_sh1106.cpp
+++ src/mgos_arduino_sh1106.cpp
@@ -7,216 +7,221 @@
 
 #include "Adafruit_SH1106.h"
 
-Adafruit_SH1106 *mgos_sh1106_create_i2c(int w, int h) {
-  return new Adafruit_SH1106(w, h);
-}
+extern "C"
+{
 
-void mgos_sh1106_close(Adafruit_SH1106 *ssd) {
-  if (ssd != nullptr) {
-    delete ssd;
-    ssd = nullptr;
+  Adafruit_SH1106 *mgos_sh1106_create_i2c(int w, int h) {
+    return new Adafruit_SH1106(w, h);
   }
-}
 
-void mgos_sh1106_begin(Adafruit_SH1106 *ssd, int vccstate, int i2caddr) {
-  if (ssd == nullptr) return;
-  ssd->begin(vccstate, i2caddr);
-}
+  void mgos_sh1106_close(Adafruit_SH1106 *ssd) {
+    if (ssd != nullptr) {
+      delete ssd;
+      ssd = nullptr;
+    }
+  }
 
-void mgos_sh1106_command(Adafruit_SH1106 *ssd, int c) {
-  if (ssd == nullptr) return;
-  ssd->SH1106_command(c);
-}
+  void mgos_sh1106_begin(Adafruit_SH1106 *ssd, int vccstate, int i2caddr) {
+    if (ssd == nullptr) return;
+    ssd->begin(vccstate, i2caddr);
+  }
 
-void mgos_sh1106_clear_display(Adafruit_SH1106 *ssd) {
-  if (ssd == nullptr) return;
-  ssd->clearDisplay();
-}
+  void mgos_sh1106_command(Adafruit_SH1106 *ssd, int c) {
+    if (ssd == nullptr) return;
+    ssd->SH1106_command(c);
+  }
 
-void mgos_sh1106_invert_display(Adafruit_SH1106 *ssd, int i) {
-  if (ssd == nullptr) return;
-  ssd->invertDisplay(i);
-}
+  void mgos_sh1106_clear_display(Adafruit_SH1106 *ssd) {
+    if (ssd == nullptr) return;
+    ssd->clearDisplay();
+  }
 
-void mgos_sh1106_display(Adafruit_SH1106 *ssd) {
-  if (ssd == nullptr) return;
-  ssd->display();
-}
+  void mgos_sh1106_invert_display(Adafruit_SH1106 *ssd, int i) {
+    if (ssd == nullptr) return;
+    ssd->invertDisplay(i);
+  }
 
-// void mgos_sh1106_start_scroll_right(Adafruit_SH1106 *ssd, int start,
-//                                      int stop) {
-//   if (ssd == nullptr) return;
-//   ssd->startscrollright(start, stop);
-// }
+  void mgos_sh1106_display(Adafruit_SH1106 *ssd) {
+    if (ssd == nullptr) return;
+    ssd->display();
+  }
 
-// void mgos_sh1106_start_scroll_left(Adafruit_SH1106 *ssd, int start,
-//                                     int stop) {
-//   if (ssd == nullptr) return;
-//   ssd->startscrollleft(start, stop);
-// }
+  // void mgos_sh1106_start_scroll_right(Adafruit_SH1106 *ssd, int start,
+  //                                      int stop) {
+  //   if (ssd == nullptr) return;
+  //   ssd->startscrollright(start, stop);
+  // }
 
-// void mgos_sh1106_start_scroll_diag_right(Adafruit_SH1106 *ssd, int start,
-//                                           int stop) {
-//   if (ssd == nullptr) return;
-//   ssd->startscrolldiagright(start, stop);
-// }
+  // void mgos_sh1106_start_scroll_left(Adafruit_SH1106 *ssd, int start,
+  //                                     int stop) {
+  //   if (ssd == nullptr) return;
+  //   ssd->startscrollleft(start, stop);
+  // }
 
-// void mgos_sh1106_start_scroll_diag_left(Adafruit_SH1106 *ssd, int start,
-//                                          int stop) {
-//   if (ssd == nullptr) return;
-//   ssd->startscrolldiagleft(start, stop);
-// }
+  // void mgos_sh1106_start_scroll_diag_right(Adafruit_SH1106 *ssd, int start,
+  //                                           int stop) {
+  //   if (ssd == nullptr) return;
+  //   ssd->startscrolldiagright(start, stop);
+  // }
 
-// void mgos_sh1106_stop_scroll(Adafruit_SH1106 *ssd) {
-//   if (ssd == nullptr) return;
-//   ssd->stopscroll();
-// }
+  // void mgos_sh1106_start_scroll_diag_left(Adafruit_SH1106 *ssd, int start,
+  //                                          int stop) {
+  //   if (ssd == nullptr) return;
+  //   ssd->startscrolldiagleft(start, stop);
+  // }
 
-// void mgos_sh1106_dim(Adafruit_SH1106 *ssd, int dim) {
-//   if (ssd == nullptr) return;
-//   ssd->dim(dim);
-// }
+  // void mgos_sh1106_stop_scroll(Adafruit_SH1106 *ssd) {
+  //   if (ssd == nullptr) return;
+  //   ssd->stopscroll();
+  // }
 
-void mgos_sh1106_draw_pixel(Adafruit_SH1106 *ssd, int x, int y, int color) {
-  if (ssd == nullptr) return;
-  ssd->drawPixel(x, y, color);
-}
+  // void mgos_sh1106_dim(Adafruit_SH1106 *ssd, int dim) {
+  //   if (ssd == nullptr) return;
+  //   ssd->dim(dim);
+  // }
 
-void mgos_sh1106_draw_fast_vline(Adafruit_SH1106 *ssd, int x, int y, int h,
-                                  int color) {
-  if (ssd == nullptr) return;
-  ssd->drawFastVLine(x, y, h, color);
-}
+  void mgos_sh1106_draw_pixel(Adafruit_SH1106 *ssd, int x, int y, int color) {
+    if (ssd == nullptr) return;
+    ssd->drawPixel(x, y, color);
+  }
 
-void mgos_sh1106_draw_fast_hline(Adafruit_SH1106 *ssd, int x, int y, int w,
-                                  int color) {
-  if (ssd == nullptr) return;
-  ssd->drawFastHLine(x, y, w, color);
-}
+  void mgos_sh1106_draw_fast_vline(Adafruit_SH1106 *ssd, int x, int y, int h,
+                                    int color) {
+    if (ssd == nullptr) return;
+    ssd->drawFastVLine(x, y, h, color);
+  }
 
-// Adafruit_GFX
-int mgos_sh1106_make_xy_pair(int x, int y) {
-  return ((int) x << 16) + y;
-}
+  void mgos_sh1106_draw_fast_hline(Adafruit_SH1106 *ssd, int x, int y, int w,
+                                    int color) {
+    if (ssd == nullptr) return;
+    ssd->drawFastHLine(x, y, w, color);
+  }
 
-void mgos_sh1106_draw_circle(Adafruit_SH1106 *ssd, int x0y0, int r,
-                              int color) {
-  if (ssd == nullptr) return;
-  ssd->drawCircle((x0y0 >> 16), x0y0, r, color);
-}
+  // Adafruit_GFX
+  int mgos_sh1106_make_xy_pair(int x, int y) {
+    return ((int) x << 16) + y;
+  }
 
-void mgos_sh1106_draw_circle_helper(Adafruit_SH1106 *ssd, int x0y0, int r,
-                                     int cornername, int color) {
-  if (ssd == nullptr) return;
-  ssd->drawCircleHelper((x0y0 >> 16), x0y0, r, cornername, color);
-}
+  void mgos_sh1106_draw_circle(Adafruit_SH1106 *ssd, int x0y0, int r,
+                                int color) {
+    if (ssd == nullptr) return;
+    ssd->drawCircle((x0y0 >> 16), x0y0, r, color);
+  }
 
-void mgos_sh1106_fill_circle(Adafruit_SH1106 *ssd, int x0y0, int r,
-                              int color) {
-  if (ssd == nullptr) return;
-  ssd->fillCircle((x0y0 >> 16), x0y0, r, color);
-}
+  void mgos_sh1106_draw_circle_helper(Adafruit_SH1106 *ssd, int x0y0, int r,
+                                      int cornername, int color) {
+    if (ssd == nullptr) return;
+    ssd->drawCircleHelper((x0y0 >> 16), x0y0, r, cornername, color);
+  }
 
-void mgos_sh1106_fill_circle_helper(Adafruit_SH1106 *ssd, int x0y0, int r,
-                                     int cornername, int delta, int color) {
-  if (ssd == nullptr) return;
-  ssd->fillCircleHelper((x0y0 >> 16), x0y0, r, cornername, delta, color);
-}
+  void mgos_sh1106_fill_circle(Adafruit_SH1106 *ssd, int x0y0, int r,
+                                int color) {
+    if (ssd == nullptr) return;
+    ssd->fillCircle((x0y0 >> 16), x0y0, r, color);
+  }
 
-void mgos_sh1106_draw_triangle(Adafruit_SH1106 *ssd, int x0y0, int x1y1,
-                                int x2y2, int color) {
-  if (ssd == nullptr) return;
-  ssd->drawTriangle((x0y0 >> 16), x0y0, (x1y1 >> 16), x1y1, (x2y2 >> 16), x2y2,
-                    color);
-}
+  void mgos_sh1106_fill_circle_helper(Adafruit_SH1106 *ssd, int x0y0, int r,
+                                      int cornername, int delta, int color) {
+    if (ssd == nullptr) return;
+    ssd->fillCircleHelper((x0y0 >> 16), x0y0, r, cornername, delta, color);
+  }
 
-void mgos_sh1106_fill_triangle(Adafruit_SH1106 *ssd, int x0y0, int x1y1,
-                                int x2y2, int color) {
-  if (ssd == nullptr) return;
-  ssd->fillTriangle((x0y0 >> 16), x0y0, (x1y1 >> 16), x1y1, (x2y2 >> 16), x2y2,
-                    color);
-}
+  void mgos_sh1106_draw_triangle(Adafruit_SH1106 *ssd, int x0y0, int x1y1,
+                                  int x2y2, int color) {
+    if (ssd == nullptr) return;
+    ssd->drawTriangle((x0y0 >> 16), x0y0, (x1y1 >> 16), x1y1, (x2y2 >> 16), x2y2,
+                      color);
+  }
 
-void mgos_sh1106_draw_round_rect(Adafruit_SH1106 *ssd, int x0y0, int w, int h,
-                                  int radius, int color) {
-  if (ssd == nullptr) return;
-  ssd->drawRoundRect((x0y0 >> 16), x0y0, w, h, radius, color);
-}
+  void mgos_sh1106_fill_triangle(Adafruit_SH1106 *ssd, int x0y0, int x1y1,
+                                  int x2y2, int color) {
+    if (ssd == nullptr) return;
+    ssd->fillTriangle((x0y0 >> 16), x0y0, (x1y1 >> 16), x1y1, (x2y2 >> 16), x2y2,
+                      color);
+  }
 
-void mgos_sh1106_fill_round_rect(Adafruit_SH1106 *ssd, int x0y0, int w, int h,
-                                  int radius, int color) {
-  if (ssd == nullptr) return;
-  ssd->fillRoundRect((x0y0 >> 16), x0y0, w, h, radius, color);
-}
+  void mgos_sh1106_draw_round_rect(Adafruit_SH1106 *ssd, int x0y0, int w, int h,
+                                    int radius, int color) {
+    if (ssd == nullptr) return;
+    ssd->drawRoundRect((x0y0 >> 16), x0y0, w, h, radius, color);
+  }
 
-void mgos_sh1106_draw_char(Adafruit_SH1106 *ssd, int xy, int c, int color,
-                            int bg, int size) {
-  if (ssd == nullptr) return;
-  ssd->drawChar((xy >> 16), xy, c, color, bg, size);
-}
+  void mgos_sh1106_fill_round_rect(Adafruit_SH1106 *ssd, int x0y0, int w, int h,
+                                    int radius, int color) {
+    if (ssd == nullptr) return;
+    ssd->fillRoundRect((x0y0 >> 16), x0y0, w, h, radius, color);
+  }
 
-void mgos_sh1106_set_cursor(Adafruit_SH1106 *ssd, int x, int y) {
-  if (ssd == nullptr) return;
-  ssd->setCursor(x, y);
-}
+  void mgos_sh1106_draw_char(Adafruit_SH1106 *ssd, int xy, int c, int color,
+                              int bg, int size) {
+    if (ssd == nullptr) return;
+    ssd->drawChar((xy >> 16), xy, c, color, bg, size);
+  }
 
-void mgos_sh1106_set_text_color(Adafruit_SH1106 *ssd, int c) {
-  if (ssd == nullptr) return;
-  ssd->setTextColor(c);
-}
+  void mgos_sh1106_set_cursor(Adafruit_SH1106 *ssd, int x, int y) {
+    if (ssd == nullptr) return;
+    ssd->setCursor(x, y);
+  }
 
-void mgos_sh1106_set_text_color_bg(Adafruit_SH1106 *ssd, int c, int bg) {
-  if (ssd == nullptr) return;
-  ssd->setTextColor(c, bg);
-}
+  void mgos_sh1106_set_text_color(Adafruit_SH1106 *ssd, int c) {
+    if (ssd == nullptr) return;
+    ssd->setTextColor(c);
+  }
 
-void mgos_sh1106_set_text_size(Adafruit_SH1106 *ssd, int s) {
-  if (ssd == nullptr) return;
-  ssd->setTextSize(s);
-}
+  void mgos_sh1106_set_text_color_bg(Adafruit_SH1106 *ssd, int c, int bg) {
+    if (ssd == nullptr) return;
+    ssd->setTextColor(c, bg);
+  }
 
-void mgos_sh1106_set_text_wrap(Adafruit_SH1106 *ssd, boolean w) {
-  if (ssd == nullptr) return;
-  ssd->setTextWrap(w);
-}
+  void mgos_sh1106_set_text_size(Adafruit_SH1106 *ssd, int s) {
+    if (ssd == nullptr) return;
+    ssd->setTextSize(s);
+  }
 
-int mgos_sh1106_write(Adafruit_SH1106 *ssd, const char *buffer, int size) {
-  if (ssd == nullptr) return 0;
-  return ssd->Print::write(buffer, size);
-}
+  void mgos_sh1106_set_text_wrap(Adafruit_SH1106 *ssd, boolean w) {
+    if (ssd == nullptr) return;
+    ssd->setTextWrap(w);
+  }
 
-int mgos_sh1106_height(Adafruit_SH1106 *ssd) {
-  if (ssd == nullptr) return 0;
-  return ssd->height();
-}
+  int mgos_sh1106_write(Adafruit_SH1106 *ssd, const char *buffer, int size) {
+    if (ssd == nullptr) return 0;
+    return ssd->Print::write(buffer, size);
+  }
 
-int mgos_sh1106_width(Adafruit_SH1106 *ssd) {
-  if (ssd == nullptr) return 0;
-  return ssd->width();
-}
+  int mgos_sh1106_height(Adafruit_SH1106 *ssd) {
+    if (ssd == nullptr) return 0;
+    return ssd->height();
+  }
 
-int mgos_sh1106_get_rotation(Adafruit_SH1106 *ssd) {
-  if (ssd == nullptr) return 0;
-  return ssd->getRotation();
-}
+  int mgos_sh1106_width(Adafruit_SH1106 *ssd) {
+    if (ssd == nullptr) return 0;
+    return ssd->width();
+  }
 
-void mgos_sh1106_set_rotation(Adafruit_SH1106 *ssd, int rotation) {
-  if (ssd == nullptr) return;
-  ssd->setRotation(rotation);
-}
+  int mgos_sh1106_get_rotation(Adafruit_SH1106 *ssd) {
+    if (ssd == nullptr) return 0;
+    return ssd->getRotation();
+  }
 
-// get current cursor position (get rotation safe maximum values, using: width()
-// for x, height() for y)
-int mgos_sh1106_get_cursor_x(Adafruit_SH1106 *ssd) {
-  if (ssd == nullptr) return 0;
-  return ssd->getCursorX();
-}
+  void mgos_sh1106_set_rotation(Adafruit_SH1106 *ssd, int rotation) {
+    if (ssd == nullptr) return;
+    ssd->setRotation(rotation);
+  }
 
-int mgos_sh1106_get_cursor_y(Adafruit_SH1106 *ssd) {
-  if (ssd == nullptr) return 0;
-  return ssd->getCursorY();
-}
+  // get current cursor position (get rotation safe maximum values, using: width()
+  // for x, height() for y)
+  int mgos_sh1106_get_cursor_x(Adafruit_SH1106 *ssd) {
+    if (ssd == nullptr) return 0;
+    return ssd->getCursorX();
+  }
 
-bool mgos_arduino_sh1106_init(void){
-    return true;
-}
+  int mgos_sh1106_get_cursor_y(Adafruit_SH1106 *ssd) {
+    if (ssd == nullptr) return 0;
+    return ssd->getCursorY();
+  }
+
+  bool mgos_arduino_sh1106_init(void){
+      return true;
+  }
+
+}//extern C
